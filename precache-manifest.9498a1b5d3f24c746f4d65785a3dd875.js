0d8a7baf36d8eeddb892c885aefce6e7a31feb27
yuri <uu5474@gmail.com>
2020-05-02 12:19:55 +0900

    Updates

---
+++ precache-manifest.9498a1b5d3f24c746f4d65785a3dd875.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "49c660e8155f5053f197202ca9e5b85c",
+    "url": "/movie_app/index.html"
+  },
+  {
+    "revision": "5a46c7ff87fc9e941c5d",
+    "url": "/movie_app/static/css/main.c912bdf8.chunk.css"
+  },
+  {
+    "revision": "70e84318d65e22caa855",
+    "url": "/movie_app/static/js/2.1c932c0d.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/movie_app/static/js/2.1c932c0d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5a46c7ff87fc9e941c5d",
+    "url": "/movie_app/static/js/main.62b2bcb9.chunk.js"
+  },
+  {
+    "revision": "ca4b155c3570fabe1dd4",
+    "url": "/movie_app/static/js/runtime-main.98d66c69.js"
+  }
+]);
